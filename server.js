1ad092b62a8dab6d85919cd570bb621ba2427ad2
khalil <ckhalil308@gmail.com>
2025-11-23 15:06:14 +0100

    deploy

--- server.js
+++ server.js
@@ -200,4 +200,7 @@ app.delete('/sites/:siteId/comments/:commentId',(req,res)=>{
 })
 
 
-app.listen(3000, () => console.log('✅ Server running at http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
